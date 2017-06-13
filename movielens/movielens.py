4f7da0872d051bba8dae65bae62e98c4fad20a0e
Gabriel Rocha <gabriel.rocha.gbr@gmail.com>
2017-06-13 11:28:35 -0300

    List reviews of movielens

--- movielens/movielens.py
+++ movielens/movielens.py
@@ -4,9 +4,19 @@ import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
-def dict_movies():
+def get_movies():
     movie = {}
     for row in open(BASE_DIR+"/data/u.item"):
         id, title = row.split("|")[0:2]
         movie[id] = title
     return movie
+
+
+def get_reviews():
+    reviews = {}
+    for row in open(BASE_DIR+"/data/u.data"):
+        user, id_movie, review = row.split("\t")[0:3]
+        reviews.setdefault(user, {})
+        reviews[user][id_movie] = float(review)
+    return reviews
+
--- tests/test_movielens.py
+++ tests/test_movielens.py
@@ -1,6 +1,12 @@
-from Code.movielens.movielens import dict_movies
+from Code.movielens.movielens import get_movies, get_reviews
 
 
-def test_from_movielens():
-    movies = dict_movies()
+def test_get_movies():
+    movies = get_movies()
     assert movies.get("1") == "Toy Story (1995)"
+
+
+def test_get_reviews():
+    reviews = get_reviews()
+    assert "242" in reviews.get("196")
+    assert reviews.get("196")["242"] == 3.0
